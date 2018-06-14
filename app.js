abdd56c07f42854c49c2789560606b3b5b69be45
Manuel Savino <msavino@fiu.edu>
2018-06-14 13:46:24 -0400

    changed response var

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ $(function(){
 
     $("#submit").on("click", function () { loadData($("#search").val())})
 
-    
+//    var data: {
+//         "$limit": 100,
+//             "$$app_token": "qkEdULtaZjOJu5obC0ygJ1KMT"
+//                 , "$q": query
+//                     , "$where": "deptname=''"
+//     }   
+
 
     function loadData(query){
         $("#tbody").empty()
@@ -12,27 +18,24 @@ $(function(){
         $.ajax({
         url: "https://opendata.miamidade.gov/resource/uxti-s6ix.json",
         method: "GET",
-        data: {
-            "$limit": 100,
-            "$$app_token": "qkEdULtaZjOJu5obC0ygJ1KMT"
-            , "$q": query
-            // ,"$where": "deptname=''"
-        }   
+       
+    }).then(function(response){
+        console.log(response)
 
-    }).then(function(responce){
-        console.log(responce)
-
-        for(i= 0; i < responce.length; i++)
-        {
-            $("#tbody").append(
-            $("<tr>")
-            .append($("<td>").append(responce[i].firstname))
-            .append($("<td>").append(responce[i].lastname))
-            .append($("<td>").append(responce[i].deptname))
-            .append($("<td>").append(responce[i].title))
-            .append($("<td>").append("$"+responce[i].annualsalary))
-            )
-
+        for (var i = 0; i < arr.length; i++) {
+            authors.push(arr[i].byline.original)
+            snippet.push(arr[i].snippet)
+            url.push(arr[i].web_url)
+            newDiv = $("<div/>", { "class": "card" })
+            newLink = $("<a>")
+            snippetPar = $("<p></p>")
+            newDiv.append(authors[i] + "<br>")
+            snippetPar.text(snippet[i])
+            newDiv.append(snippet[i] + "<br>")
+            newLink.attr("href", url[i])
+            newLink.text("Link")
+            newDiv.append(newLink)
+            $("#results-div").append(newDiv)
         }
     })
 }
